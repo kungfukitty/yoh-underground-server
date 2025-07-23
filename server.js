2a463975700e4485874b68adec834dd4cd6cb629
kungfukitty <46254915+kungfukitty@users.noreply.github.com>
2025-07-23 17:20:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-// Register routes
 app.use('/api/member', memberRoutes);
 
-// Root route
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+app.get('/', (req, res) => res.send('API is running...'));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
